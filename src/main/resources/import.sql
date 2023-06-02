8a71893160474e4dcb33ae5f5451b0bdebe44292
jesusbeb <jesusbelb@gmail.com>
2023-06-02 13:20:36 -0600

    Creando la vista listar.html

---
+++ src/main/resources/import.sql
@@ -0,0 +1,3 @@
+/*Importante que este archivo se llame import.sql*/
+INSERT INTO clientes (id, nombre, apellido, email, create_at) VALUES(1, 'Jesus', 'Beltran', 'jesus@mail.com', '2023-06-02');
+INSERT INTO clientes (id, nombre, apellido, email, create_at) VALUES(2, 'Andres', 'Guzman', 'andres@mail.com', '2017-08-20');
